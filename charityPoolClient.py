f73eb3fd30b4ce892a34bcc693883084297807fc
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-11-29 09:02:14 +0000

    Code cleanup

--- charityPoolClient.py
+++ charityPoolClient.py
@@ -1,6 +1,6 @@
 import sys
 import json
-import os
+
 #import from the 21 Developer Library
 from two1.commands.config import Config
 from two1.lib.wallet import Wallet
@@ -12,29 +12,29 @@ username = Config().username
 requests = BitTransferRequests(wallet, username)
 
 # server address
-def donate(server_url = 'http://localhost:9393/'):
-
-    # get the file listing from the server
+def donate(server_url):
+    # get the charity listings from the server
     response = requests.get(url=server_url+'charities')
-    file_list = json.loads(response.text)
-    for file in range(len(file_list)):
-        print("{}".format(file_list[file]["name"]))
+    charity_list = json.loads(response.text)
+    for charity_index in range(len(charity_list)):
+        print("{0}. {1}".format(charity_index + 1, charity_list[charity_index]["name"]))
     try:
-        # prompt the user to input the index number of the file to be purchase
-        sel = input("Please enter the index of the charity you'd like to donate to: ")
-        donation_amount = int(input("How much would you like to donate to this charity?: "))
-        # check if the input index is valid key in file_list dict
-        if int(sel) <= len(file_list):
-            print('You selected {} in our database'.format(file_list[int(sel) - 1]["name"]))
+        # prompt the user to input the index number of the charity to donate to and the amount to donate
+        sel = int(input("Please enter the index of the charity you'd like to donate to: "))
+        # check if the input index is valid key in charity_list dict
+        if sel <= len(charity_list):
+            charity_name = charity_list[sel-1]["name"]
+            donation_amount = int(input("How many satoshis would you like to donate to {}?: ".format(charity_name)))
         else:
             print("That is an invalid selection.")
-        #create a 402 request with the server payout address
-        sel_url = server_url+'donate?donation={0}'
-        answer = requests.get(url=sel_url.format(donation_amount), stream=True)
+            return
+        #create a 402 request
+        sel_url = server_url+'donate?sel={0}&donation={1}'.format(sel, donation_amount)
+        answer = requests.get(url=sel_url, stream=True)
         if answer.status_code != 200:
-                print("Could not make an offchain payment. Please check that you have sufficient balance.")
+            print("Could not make an offchain payment. Please check that you have sufficient balance.")
         else:
-            print('Congratulations, you just donated to charity!')
+            print('Congratulations, you just donated {0} satoshis to {1}! Thanks!'.format(donation_amount, charity_name))
 
 
     except ValueError:
